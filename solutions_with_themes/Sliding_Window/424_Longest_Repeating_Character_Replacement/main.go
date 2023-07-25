5b5a8924944982e4f2019ce0377facf790291b67
arevbond <bondarev.n17@yandex.ru>
2023-07-25 12:42:37 +0300

    Solved #424 problem

--- solutions_with_themes/Sliding_Window/424_Longest_Repeating_Character_Replacement/main.go
+++ solutions_with_themes/Sliding_Window/424_Longest_Repeating_Character_Replacement/main.go
@@ -2,16 +2,34 @@ package main
 
 import "fmt"
 
-func characterReplacement(s string, k int) int {
-    maxLen, curLen := 1, 1
-    l, r := 0, 1 
-    for ; r < len(s) && k > 0; r++ {
-    	
+func max(a, b int) int {
+    if a > b {
+        return a
     }
+    return b
+}
+
+func characterReplacement(s string, k int) int {
+    // maxLen = 0
+    dic := make(map[byte]int)
+    res := 0
+    l, r, mostF := 0, 0, 0
+    for ; r < len(s); r++ {
+    	dic[s[r]] += 1
+        
+        mostF = max(mostF, dic[s[r]])
+
+        if (r - l + 1) - mostF > k {
+            dic[s[l]]--
+            l++
+        }
+        res = max(res, r - l +1)
+    }
+    return res
 }
 
 
 func main() {
 	fmt.Println(characterReplacement("ABAB", 2))
-	fmt.Println(characterReplacement("AABABBA", 1))
+	// fmt.Println(characterReplacement("AABABBA", 1))
 }
