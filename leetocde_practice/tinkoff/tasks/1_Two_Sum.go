3c457feed53aaaccb687ec3bf179b6dee0aaf000
arevbond <bondarev.n17@yandex.ru>
2025-07-17 12:18:45 +0300

    Solve №1 problem

---
+++ leetocde_practice/tinkoff/tasks/1_Two_Sum.go
@@ -0,0 +1,14 @@
+func twoSum(nums []int, target int) []int {
+	// number: indx
+	mp := make(map[int]int)
+
+	for i, num := range nums {
+		if prevIndx, ok := mp[target-num]; ok {
+			return []int{prevIndx, i}
+		}
+
+		mp[num] = i
+	}
+
+	return nil
+}
